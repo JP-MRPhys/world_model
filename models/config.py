467f3706a3fd5ef664e2414e6dac35c34f101177
jehill <jehillparikh@gmail.com>
2020-06-25 23:25:01 +0100

    ad

---
+++ models/config.py
@@ -0,0 +1,48 @@
+import numpy as np
+import random
+
+train_envs = ['fastmri']
+test_envs = ['fastmri']
+
+def generate_data_action(t, env):
+
+    a = env.action_space.sample()
+
+
+    # if t < 20:
+    #     a = np.array([-0.1,1,0])
+
+    # else:  
+    #     a = env.action_space.sample()
+
+    #     rn = random.randint(0,9)
+
+    #     if rn in [0]:
+    #         a = np.array([0,0,0])
+    #     elif rn in [1,2,3,4]:
+    #         a = np.array([0,random.random(),0])
+    #     elif rn in [5,6]:
+    #         a = np.array([-random.random(),0,0])
+    #     elif rn in [7,8]:
+    #         a = np.array([random.random(),0,0])
+    #     elif rn in [9]:
+    #         a = np.array([0,0,random.random()])
+    #     else:
+    #         pass
+
+    return a
+
+
+def adjust_obs(obs):
+    # obs[obs==0] = 255
+    
+    #return obs.astype('float32') / 255.
+    return obs
+
+def adjust_reward(reward):
+    if reward > 0:
+        reward = 1
+    else:
+        reward = 0
+    return reward
+    
