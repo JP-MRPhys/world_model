a5d849cde55154b76674cafc4e298fc53a2177c0
jehill <jehillparikh@gmail.com>
2020-07-15 14:43:55 +0100

    Add json save for models.

--- models/world_model.py
+++ models/world_model.py
@@ -222,7 +222,7 @@ def simulate(model, num_episode=5, seed=-1, max_len=-1, generate_data_mode=False
             #                np.array([model.cell_values])]
 
 
-            a=np.concatenate([vae_encoded_obs, action, [reward]])
+            a=np.concatenate([np.squeeze(vae_encoded_obs), action, [reward]])
             print(np.size(a))
 
             y_pred_rnn, rnn_hidden, rnn_cell = model.rnn.predict(a, np.array(model.hidden), np.array(model.cell_values))
