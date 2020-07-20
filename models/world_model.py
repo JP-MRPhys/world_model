6192f0cd09be1f240de715c7afc57dd43c332e85
jehill <jehillparikh@gmail.com>
2020-07-20 19:11:29 +0100

    changing model running features

--- models/world_model.py
+++ models/world_model.py
@@ -254,8 +254,8 @@ def simulate(model, num_episode=5, seed=-1, max_len=-1, generate_data_mode=False
             print(np.shape(actions))
 
 
-            #merged_input=np.concatenate([np.squeeze(vae_encoded_obs), [action], [reward]])
-            merged_input=np.concatenate([vae_encoded_obs, actions, rewards])
+            merged_input=np.concatenate([np.squeeze(vae_encoded_obs), [action], [reward]])
+            #merged_input=np.concatenate([vae_encoded_obs, actions, rewards])
             print(merged_input)
 
             merged_input = np.reshape(merged_input, [1, 1, np.shape(merged_input)[0]])
